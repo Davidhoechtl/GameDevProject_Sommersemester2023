81cbdc8d5774b7db9ab009e2267bdc0d6db3e9e0
Davidhoechtl <davidhoechtl.job@gmail.com>
2023-05-09 15:50:50 +0200

    Added basic player controller

---
+++ minigame-template-main/Assets/YourMinigameName/Code/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float Speed = 200f;
+
+    public float horizontalInput;
+    public float verticalInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontalInput, 0, verticalInput).normalized;
+        playerRb.AddForce(movement * Speed * Time.deltaTime, ForceMode.Force);
+    }
+
+    private Rigidbody playerRb;
+}
