f940c3325c711f609c6feadf62cecffa3a21688e
oliver13600 <82397911+oliver13600@users.noreply.github.com>
2023-05-26 02:38:34 +0200

    Player Manager

--- minigame-template-main/Assets/YourMinigameName/Code/Scripts/PlayerController.cs
+++ minigame-template-main/Assets/YourMinigameName/Code/Scripts/PlayerController.cs
@@ -6,38 +6,14 @@ namespace Assets.YourMinigameName.Code.Scripts
 
     public class PlayerController : MonoBehaviour
     {
-        public float Speed = 200f;
+        public float speed = 20;
+        private Vector2 movementInput;
 
-        public float horizontalInput;
-        public float verticalInput;
-
-        private Vector2 moveDirection = Vector2.zero;
-
-        public InputAction playerInput;
-        // Start is called before the first frame update
-        void Start()
+        private void Update()
         {
-            playerRb = GetComponent<Rigidbody>();
+            transform.Translate(new Vector3 (movementInput.x, 0, movementInput.y) * speed * Time.deltaTime);
         }
 
-        private void OnEnable()
-        {
-            playerInput.Enable();
-        }
-
-        private void OnDisable()
-        {
-            playerInput.Disable();
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-            moveDirection = playerInput.ReadValue<Vector2>();
-            Vector3 movement = new Vector3(moveDirection.x, 0, moveDirection.y).normalized;
-            playerRb.AddForce(movement * Speed * Time.deltaTime, ForceMode.Force);
-        }
-
-        private Rigidbody playerRb;
+        public void OnMove(InputAction.CallbackContext ctx) => movementInput = ctx.ReadValue<Vector2>();
     }
 }
