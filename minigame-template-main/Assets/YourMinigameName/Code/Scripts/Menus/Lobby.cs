5870bf32c298fd31372fdd92f08a55c286adc952
MrGreenMeme <lorenzflich@gmail.com>
2023-05-31 11:04:36 +0200

    MainMenu updated

--- minigame-template-main/Assets/YourMinigameName/Code/Scripts/Menus/Lobby.cs
+++ minigame-template-main/Assets/YourMinigameName/Code/Scripts/Menus/Lobby.cs
@@ -10,6 +10,22 @@ public class Lobby : MonoBehaviour
     public Image image;
     public TMP_Text playerText;
 
+    //UI
+    public int currentIndex = 0;
+    private int arrayLength;
+    private float swapTimer = 3;
+    [SerializeField] Selectable[] uiComponents;
+
+    private void Start()
+    {
+        arrayLength = uiComponents.Length;
+    }
+
+    private void Update()
+    {
+        swapTimer -= Time.deltaTime;
+    }
+
     public void JoinLobby(InputAction.CallbackContext ctx)
     {
         //image.color = new Color(68, 153, 68, 1); // 20, 255, 12, 256
@@ -24,6 +40,38 @@ public class Lobby : MonoBehaviour
         image.GetComponent<Image>().color = new Color32(255, 255, 255, 255);
         playerText.text = "Press \"Rt\" to Join";
         Debug.Log("Leave Lobby");
+    }
+
+    public void IncreaseIndex(InputAction.CallbackContext ctx)
+    {
+        if (swapTimer <= 0)
+        {
+            currentIndex++;
+            Debug.Log("Increased Index");
+            if (currentIndex >= arrayLength)
+            {
+                currentIndex = 0;
+            }
+            uiComponents[currentIndex].Select();
+            swapTimer = 3;
+        }
+
+    }
+
+    public void DecreaseIndex(InputAction.CallbackContext ctx)
+    {
+        if (swapTimer <= 0)
+        {
+            currentIndex--;
+            Debug.Log("Decreased Index");
+            if (currentIndex < 0)
+            {
+                currentIndex = arrayLength - 1;
+            }
+            uiComponents[currentIndex].Select();
+            swapTimer = 3;
+        }
+
     }
 
 }
