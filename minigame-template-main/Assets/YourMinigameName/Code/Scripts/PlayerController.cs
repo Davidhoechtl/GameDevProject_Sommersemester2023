ebc73d9592013ecff600318fd32a847b4f70b4ba
oliver13600 <82397911+oliver13600@users.noreply.github.com>
2023-06-01 19:26:20 +0200

    player controller MVP

--- minigame-template-main/Assets/YourMinigameName/Code/Scripts/PlayerController.cs
+++ minigame-template-main/Assets/YourMinigameName/Code/Scripts/PlayerController.cs
@@ -3,12 +3,18 @@ namespace Assets.YourMinigameName.Code.Scripts
 {
     using UnityEngine;
     using UnityEngine.InputSystem;
+    using System.Collections;
+
 
     public class PlayerController : MonoBehaviour
     {
-        public float speed = 20;
-        public float jumpAmount = 10;
+        public float speed = 0.1f;
+        public float jumpAmount = 5;
         private Vector2 movementInput;
+        private bool isReady2Jump = true;
+        private bool canMove = true;
+        private int collisionCount = 0;
+        //private int jumpCount = 3;
 
 
         public Rigidbody rb;
@@ -21,7 +27,10 @@ namespace Assets.YourMinigameName.Code.Scripts
 
         private void Update()
         {
-            transform.Translate(new Vector3 (movementInput.x, 0, movementInput.y) * speed * Time.deltaTime);
+            if(canMove && (collisionCount > 0))
+            {
+                transform.Translate(new Vector3(movementInput.x, 0, movementInput.y) * speed * Time.deltaTime);
+            }
         }
 
         public void OnMove(InputAction.CallbackContext ctx)
@@ -36,10 +45,48 @@ namespace Assets.YourMinigameName.Code.Scripts
 
         public void OnJump(InputAction.CallbackContext ctx)
         {
-            rb.AddForce(Vector3.up * jumpAmount, ForceMode.Impulse);
-            
+            if (isReady2Jump)
+            {
+                Vector3 jumpForce = (Vector3.up + transform.forward * 0.2f) * jumpAmount;
+                rb.AddForce(jumpForce, ForceMode.Impulse);
+                isReady2Jump = false;
+                StartCoroutine(Wait(2));
+            }
         }
 
-    
+        IEnumerator Wait(float time)
+        {
+            yield return new WaitForSecondsRealtime(time);
+
+            isReady2Jump = true;
+        }
+
+        public void SetCanMove(bool value)
+        {
+            canMove = value;
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.collider.CompareTag("Cube"))
+            {
+                collisionCount++;
+                SetCanMove(true);
+            }
+        }
+
+        private void OnCollisionExit(Collision collision)
+        {
+            if (collision.collider.CompareTag("Cube"))
+            {
+                collisionCount--;
+                if (collisionCount == 0)
+                {
+                    SetCanMove(false);
+                }
+            }
+        }
+
+
     }
 }
